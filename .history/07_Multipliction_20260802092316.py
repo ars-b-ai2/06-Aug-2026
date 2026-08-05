9e14694fc633755d40ba72e94940951256042b2b
ars-b-ai29 <arsb.ai29@gmail.com>
2026-08-05 23:07:51 +0530

    Initial commit

---
+++ .history/07_Multipliction_20260802092316.py
@@ -0,0 +1,8 @@
+# Multiplication Table of a Number
+
+num = int(input("Enter a number: "))
+
+print(f"\nMultiplication Table of {num}")
+
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
---
+++ .history/07_Multipliction_20260802093928.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(n, "x", i, "=", n * i)
+    
+    
+    
+# # Multiplication Table of a Number
+
+# num = int(input("Enter a number: "))
+
+# print(f"\nMultiplication Table of {num}")
+
+# for i in range(1, 11):
+#     print(f"{num} x {i} = {num * i}")
